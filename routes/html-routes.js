3f7ab21ba08e099f3fdcc03f24b58b527ab7729e
AndyRoush <andyclan@gmail.com>
2018-05-04 21:11:58 -0700

    cleaned up some code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,16 @@
-// Dependencies
-// =============================================================
+// Dependencies ======================
 var path = require("path")
 
-// Routes
-// =============================================================
-module.exports = function(app) {
+// Routes ==============================
+module.exports = function (app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-  app.get("/", function(req, res) {
+
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/burgers.html"))
   })
 
-  // burger route loads burger.html
-  app.get("/burgers", function(req, res) {
+
+  app.get("/burgers", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/burgers.html"))
   })
 
